760039c5920692e54b7ec7f66effd16a7d65b758
taichunmin <taichunmin@gmail.com>
2022-05-30 12:24:59 +0800

    20220530

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,15 @@ const express = require('express')
 const router = express.Router()
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', async (req, res, next) => {
   res.render('index', {
     CAROUSEL_CSV: getenv('CAROUSEL_CSV', ''),
     CAROUSEL_FIELD: getenv('CAROUSEL_FIELD', ''),
   })
+})
+
+router.get('/keep-alive', async (req, res, next) => {
+  res.json({})
 })
 
 module.exports = router
